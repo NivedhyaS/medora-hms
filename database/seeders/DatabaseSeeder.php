d89110e47392dd2f25e02bd3f1251e90c2c3caca
Nivedhya S <nivedhyas2006@gmail.com>
2026-03-29 13:50:37 +0530

    Set cloud admin to admin@medora.com / admin123

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Create Your Custom Admin
+        User::create([
+            'name' => 'Medora Admin',
+            'email' => 'admin@medora.com',
+            'password' => \Illuminate\Support\Facades\Hash::make('admin123'),
+            'role' => User::ROLE_ADMIN,
         ]);
     }
 }
